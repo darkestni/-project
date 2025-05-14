1a118e791af6a7664cb2c6324294570c14a49921
xun zhao <12312606@mail.sustech.edu.cn>
2025-05-14 22:45:42 +0800

    Delete cs307 directory

--- cs307/exception/DBException.java
+++
@@ -1,9 +0,0 @@
-package edu.sustech.cs307.exception;
-
-public class DBException extends Exception {
-    public DBException(ExceptionTypes exceptionType) {
-        super(String.format(
-                "%s: %s", exceptionType.name(), exceptionType.GetErrorResult()
-        ));
-    }
-}
--- cs307/logicalOperator/dml/DMLExecutor.java
+++
@@ -1,7 +0,0 @@
-package edu.sustech.cs307.logicalOperator.dml;
-
-import edu.sustech.cs307.exception.DBException;
-
-public interface DMLExecutor {
-    void execute() throws DBException;
-}
